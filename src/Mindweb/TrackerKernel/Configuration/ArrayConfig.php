614ce1adadc3953a9873da6e262402e995f5a29f
Marcin Czołnowski <marcin@czolnowski.net>
2015-04-06 19:05:51 +0200

    Add ArrayConfig as base config for Config interface.

---
+++ src/Mindweb/TrackerKernel/Configuration/ArrayConfig.php
@@ -0,0 +1,72 @@
+<?php
+namespace Mindweb\TrackerKernel\Configuration;
+
+class ArrayConfig implements Config
+{
+    /**
+     * @var array
+     */
+    private $entries;
+
+    public function __construct()
+    {
+        $this->entries = array();
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->entries[$offset]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetGet($offset)
+    {
+        return $this->entries[$offset];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->entries[$offset] = $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetUnset($offset)
+    {
+        if (isset($this->entries[$offset])) {
+            unset ($this->entries[$offset]);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize()
+    {
+        return serialize($this->entries);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized)
+    {
+        $this->entries = unserialize($serialized);
+    }
+
+    /**
+     * @return array
+     */
+    public function asArray()
+    {
+        return $this->entries;
+    }
+} 
